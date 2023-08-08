63629510fcd2b14d6b16d966b1857a316701d6c8
AyaHassan33 <ayagohar102030@gmail.com>
2023-08-08 04:41:22 +0200

    inital commit add sebha activity

--- app/src/main/java/com/example/islamapp/MainActivity.kt
+++ app/src/main/java/com/example/islamapp/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.islamapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var btn_sebha : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        btn_sebha=findViewById(R.id.btn_tasbeh)
+        btn_sebha.setOnClickListener{
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.fragment_container,SebhaFragment())
+                .commit()
+
+        }
+
+
     }
 }
