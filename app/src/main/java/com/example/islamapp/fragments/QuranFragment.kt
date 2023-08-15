e19e9bb98f1fca05b06696bd1d365f17d5ef51a8
AyaHassan33 <ayagohar102030@gmail.com>
2023-08-15 03:16:22 +0200

    add night mood

--- app/src/main/java/com/example/islamapp/fragments/QuranFragment.kt
+++ app/src/main/java/com/example/islamapp/fragments/QuranFragment.kt
@@ -7,11 +7,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.recyclerview.widget.RecyclerView
 import com.example.islamapp.Constants
 import com.example.islamapp.R
 import com.example.islamapp.SuraDetailsActivity
-
 import com.example.islamapp.adapter.SuraRecyclerAdapter
 import com.example.islamapp.interfaces.OnSuraClickListener
 import com.example.islamapp.model.SuraNameData
@@ -20,6 +21,7 @@ import com.example.islamapp.model.arSuras
 class QuranFragment : Fragment() {
     lateinit var recyclerView: RecyclerView
     lateinit var suraRecyclerAdapter:SuraRecyclerAdapter
+    lateinit var switchMoodButton:ImageView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -47,7 +49,29 @@ class QuranFragment : Fragment() {
 
         }
         recyclerView.adapter=suraRecyclerAdapter
+        switchMoodButton=view.findViewById(R.id.switch_mood_button)
+        switchMoodButton.tag="image"
+
+
+        if(AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES){
+            switchMoodButton.setImageResource(R.drawable.ic_light_mode)
+            switchMoodButton.tag=R.drawable.ic_light_mode
+        }else{
+            switchMoodButton.setImageResource(R.drawable.ic_night_mood)
+            switchMoodButton.tag=R.drawable.ic_night_mood
+        }
+
+
+        switchMoodButton.setOnClickListener {
+            if(switchMoodButton.tag == R.drawable.ic_night_mood){
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+
+            }else if(switchMoodButton.tag == R.drawable.ic_light_mode){
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+        }
 
     }
+
 
 }
